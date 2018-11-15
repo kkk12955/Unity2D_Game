f224c8451e39789807a434b4c9cf43c59bb7dc8e
Huang-Hao-Yu <h12955@yahoo.com.tw>
2018-11-15 17:16:18 +0800

    Upload Project

---
+++ Assets/Resource/Script/AnimatorControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+public class AnimatorControl : MonoBehaviour
+{
+    private Animator animator;
+    private float speed;
+    public float minSpeed;
+    public float maxSpeed;
+    public float addSpeed;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        speed = 0;
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            speed = Mathf.MoveTowards(speed, maxSpeed, addSpeed * Time.deltaTime);
+        else
+            speed = Mathf.MoveTowards(speed, minSpeed, addSpeed * Time.deltaTime * 2);
+        animator.SetFloat("speed", speed);
+    }
+}
